a417083224057b0a7a9ad48d1e23a169a517520a
kendarorg <kendarorg@users.noreply.github.com>
2024-05-29 13:30:33 +0200

    [22] Add subscribe/unsubscribe state for resp3 (#26)

--- protocol-redis/src/main/java/org/kendar/redis/Resp3Protocol.java
+++ protocol-redis/src/main/java/org/kendar/redis/Resp3Protocol.java
@@ -5,9 +5,13 @@ import org.kendar.protocol.context.ProtoContext;
 import org.kendar.protocol.descriptor.NetworkProtoDescriptor;
 import org.kendar.protocol.descriptor.ProtoDescriptor;
 import org.kendar.protocol.events.BytesEvent;
+import org.kendar.protocol.states.special.ProtoStateSequence;
+import org.kendar.protocol.states.special.ProtoStateSwitchCase;
 import org.kendar.protocol.states.special.ProtoStateWhile;
 import org.kendar.redis.fsm.Resp3MessageTranslator;
 import org.kendar.redis.fsm.Resp3PullState;
+import org.kendar.redis.fsm.Resp3Subscribe;
+import org.kendar.redis.fsm.Resp3Unsubscribe;
 import org.kendar.redis.fsm.events.Resp3Message;
 
 import java.util.concurrent.ConcurrentHashMap;
@@ -40,16 +44,13 @@ public class Resp3Protocol extends NetworkProtoDescriptor {
     @Override
     protected void initializeProtocol() {
         addInterruptState(new Resp3MessageTranslator(BytesEvent.class));
-//        initialize(
-//            new ProtoStateWhile(
-//                    new ProtoStateSwitchCase(
-//                        new Resp3PullState(Resp3Message.class),
-//                        new ProtoStateSequence(
-//                                new Resp3Subscribe(Resp3Message.class),
-//                                new Resp3Unsubscribe(Resp3Message.class))));
         initialize(
                 new ProtoStateWhile(
-                        new Resp3PullState(Resp3Message.class)));
+                        new ProtoStateSwitchCase(
+                                new ProtoStateSequence(
+                                        new Resp3Subscribe(Resp3Message.class),
+                                        new Resp3Unsubscribe(Resp3Message.class)),
+                                new Resp3PullState(Resp3Message.class))));
 
 
     }
