1c6cc173177edca8cb5404b0eff8d01b32ad0d56
edaros <github@kendar.org>
2024-11-18 00:35:38 +0100

    [ALL] Starting matchin sql mocks

--- protocol-common/src/main/java/org/kendar/plugins/JdbcMockPlugin.java
+++ protocol-common/src/main/java/org/kendar/plugins/JdbcMockPlugin.java
@@ -2,15 +2,27 @@ package org.kendar.plugins;
 
 import org.kendar.sql.jdbc.SelectResult;
 import org.kendar.sql.jdbc.proxy.JdbcCall;
+import org.kendar.sql.parser.SqlStringParser;
 import org.kendar.utils.ChangeableReference;
 
 import java.util.List;
 
 public abstract class JdbcMockPlugin extends MockPlugin<JdbcCall, SelectResult> {
+    private final SqlStringParser parser;
+
+    public JdbcMockPlugin() {
+        super();
+        parser = new SqlStringParser(getSqlStringParserSeparator());
+    }
     @Override
     protected void checkMatching(MockStorage data, JdbcCall requestToSimulate, ChangeableReference<Integer> matchingQuery, ChangeableReference<Long> foundedIndex) {
-
+        var possibleRequest = data.retrieveInAs(JdbcCall.class);
+        var possibleTokenized = parser.parseString(possibleRequest.getQuery());
+        var requestedTokenized = parser.parseString(requestToSimulate.getQuery());
+        asdfasd
     }
+
+    protected abstract String getSqlStringParserSeparator();
 
     @Override
     protected void writeOutput(JdbcCall request, SelectResult response, MockStorage founded) {
