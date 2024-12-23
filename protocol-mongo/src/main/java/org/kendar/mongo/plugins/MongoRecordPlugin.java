1a339fe198776557b6efff381f9493ed3c9e12dc
edaros <github@kendar.org>
2024-12-23 10:36:25 +0100

    Fixed mongo serialization of documents

--- protocol-mongo/src/main/java/org/kendar/mongo/plugins/MongoRecordPlugin.java
+++ protocol-mongo/src/main/java/org/kendar/mongo/plugins/MongoRecordPlugin.java
@@ -1,10 +1,17 @@
 package org.kendar.mongo.plugins;
 
+import org.kendar.mongo.dtos.BaseMessageData;
 import org.kendar.plugins.RecordPlugin;
 import org.kendar.plugins.settings.BasicRecordPluginSettings;
 
 public class MongoRecordPlugin extends RecordPlugin<BasicRecordPluginSettings> {
-
+    @Override
+    protected Object getData(Object of) {
+        if (of instanceof BaseMessageData) {
+            return ((BaseMessageData) of).serialize();
+        }
+        return of;
+    }
 
     @Override
     public String getProtocol() {
