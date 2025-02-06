69e3552abef04e24c983470b19db17e8ca268dbc
edaros <github@kendar.org>
2025-02-06 18:06:19 +0100

    [ALL] Added apis for settings get/put [ALL] Added content dependant from method call in FilterDescriptor

--- protocol-common/src/main/java/org/kendar/di/annotations/TpmNamed.java
+++ protocol-common/src/main/java/org/kendar/di/annotations/TpmNamed.java
@@ -5,10 +5,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * Assign a base DNS/address to respond to
- * Mandatory for @FilterClass
- */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.PARAMETER})
 public @interface TpmNamed {
