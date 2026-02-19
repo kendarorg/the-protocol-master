8bb4df7b7446310e5087f8a9bd0f237ba9f1e63a
kendarorg <github@kendar.org>
2026-02-19 10:47:55 +0100

    Update pages for tls in jdbc

--- protocol-postgres/src/main/java/org/kendar/postgres/PostgresProtocolSettings.java
+++ protocol-postgres/src/main/java/org/kendar/postgres/PostgresProtocolSettings.java
@@ -5,7 +5,17 @@ import org.kendar.sql.jdbc.settings.JdbcProtocolSettings;
 
 @TpmService(tags = "postgres")
 public class PostgresProtocolSettings extends JdbcProtocolSettings {
+
+    private boolean useTls = false;
     public PostgresProtocolSettings() {
         setProtocol("postgres");
+    }
+
+    public boolean isUseTls() {
+        return useTls;
+    }
+
+    public void setUseTls(boolean useTls) {
+        this.useTls = useTls;
     }
 }
